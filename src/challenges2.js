2895fa8c7351aa31cb197ea9fa7d341fae875020
Lucas <lucasmeinen@yahoo.com.br>
2022-10-13 17:46:50 -0300

    :sparkles: feat: Função techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,15 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tecnologias, nome) {
+  if (tecnologias.length === 0) {
+    return 'Vazio!';
+  }
+  let listaTechOrd = tecnologias.sort();
+  let listaTech = [];
+  for (let i = 0; i < tecnologias.length; i += 1) {
+    let objeto = { tech: listaTechOrd[i], name: nome };
+    listaTech.push(objeto);
+  }
+  return listaTech;
 }
 
 // Desafio 11
