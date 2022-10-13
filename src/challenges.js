069da0d3841d13a01563cc8f0dd013f489bd3174
Lucas <lucasmeinen@yahoo.com.br>
2022-10-13 16:04:24 -0300

    :sparkles: feat: Função splitSentence

--- src/challenges.js
+++ src/challenges.js
@@ -9,8 +9,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let divide = string.split(' ');
+  return divide;
 }
 
 // Desafio 4
